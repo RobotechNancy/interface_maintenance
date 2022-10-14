639958003eadbbec4f131fb9c3746d709f52a1b7
HeatSink Ru <heatsink.ru@gmail.com>
2022-10-14 20:09:47 +0200

    init repo with blank laravel install

---
+++ lang/fr/auth.php
@@ -0,0 +1,20 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Authentication Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used during authentication for various
+    | messages that we need to display to the user. You are free to modify
+    | these language lines according to your application's requirements.
+    |
+    */
+
+    'failed' => 'Les identifiants n\'existent pas dans la base de données.',
+    'password' => 'Le mot de passe entré est incorrect.',
+    'throttle' => 'Vous avez effectué un trop grand nombre de tentatives de connexion. Merci de réessayer dans :seconds secondes.',
+
+];
---
+++ lang/fr/passwords.php
@@ -0,0 +1,22 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Password Reset Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are the default lines which match reasons
+    | that are given by the password broker for a password update attempt
+    | has failed, such as for an invalid token or invalid new password.
+    |
+    */
+
+    'reset' => 'Votre mot de passe a été réinitialisé !',
+    'sent' => 'Un email contenant un lien de réinitialisation vous a été envoyé !',
+    'throttled' => 'Merci de patienter avant un nouvel essai.',
+    'token' => 'Le jeton de réinitialisation de mot de passe est incorrect.',
+    'user' => "Aucun utilisateur n'est enregistré avec cette adresse email.",
+
+];
