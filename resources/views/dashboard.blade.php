7f2a6e535637540fce54513fb04509c3c0d1694a
OtarieSupreme <gaby.mouchette@gmail.com>
2022-10-24 23:10:19 +0200

    Upadate Dpad base roulante

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,110 +1,133 @@
 <x-app-layout>
     <x-slot name="title"> @lang('Dashboard') </x-slot>
     <div class="tile is-ancestor">
-        <div class="tile is-5 is-vertical is-parent">
-          <div class="tile is-child box notification is-light">
-            <p class="title is-5">Commandes de diagnostic</p>
-            <div class="buttons">
-                <form method="POST" id="form_1">
-                    @csrf
-                    <button class="button is-link btn_form" id="btn_1" type="submit">
-                        <span>Vérification de la connectivité</span>
-                        <span class="icon">
-                            <i class="fa-solid fa-tower-cell"></i>
-                        </span>
-                    </button>
-                </form>
-                <form method="POST" id="form_2" class="ml-2">
-                    @csrf
-                    <button class="button is-link btn_form" id="btn_2" type="submit">
-                        <span>Avancer le robot</span>
-                        <span class="icon">
-                            <i class="fa-solid fa-gamepad"></i>
-                        </span>
-                    </button>
-                </form>
-                <form method="POST" id="form_3">
-                    @csrf
-                    <button class="button is-link btn_form" id="btn_3" type="submit">
-                        <span>Récupérer position robot</span>
-                        <span class="icon">
-                            <i class="fa-solid fa-crosshairs"></i>
-                        </span>
-                    </button>
-                </form>
+        <div class="tile is-4">
+            <div class="tile is-parent is-vertical">
+                <div class="tile is-child box notification is-light">
+                    <p class="title is-5">Commandes de diagnostic</p>
+                    <div class="buttons">
+                        <form method="POST" id="form_1">
+                            @csrf
+                            <button class="button is-link btn_form" id="btn_1" type="submit">
+                                <span>Vérification de la connectivité</span>
+                                <span class="icon">
+                                    <i class="fa-solid fa-tower-cell"></i>
+                                </span>
+                            </button>
+                        </form>
+                        <form method="POST" id="form_2" class="ml-2">
+                            @csrf
+                            <button class="button is-link btn_form" id="btn_2" type="submit">
+                                <span>Avancer le robot</span>
+                                <span class="icon">
+                                    <i class="fa-solid fa-gamepad"></i>
+                                </span>
+                            </button>
+                        </form>
+                        <form method="POST" id="form_3">
+                            @csrf
+                            <button class="button is-link btn_form" id="btn_3" type="submit">
+                                <span>Récupérer position robot</span>
+                                <span class="icon">
+                                    <i class="fa-solid fa-crosshairs"></i>
+                                </span>
+                            </button>
+                        </form>
 
-            </div>
-            <div class="buttons">
-                <div class="buttons">
-                    <form method="POST" id="form_4">
-                        @csrf
-                        <button class="button is-link btn_form" id="btn_3" type="submit">
-                            <span class="icon">
-                                <i class="fa-solid fa-arrow-right fa-rotate-by" style="--fa-rotate-angle: 210deg;"></i>
-                                </span>
-                        </button>
-                    </form>
-                    <form method="POST" id="form_5">
-                        @csrf
-                        <button class="button is-link btn_form" id="btn_3" type="submit">
-                            <span class="icon">
-                                <i class="fa-solid fa-arrow-right fa-rotate-by" style="--fa-rotate-angle: 270deg;"></i>
-                                </span>
-                        </button>
-                    </form>
-                    <form method="POST" id="form_6">
-                        @csrf
-                        <button class="button is-link btn_form" id="btn_3" type="submit">
-                            <span class="icon">
-                                <i class="fa-solid fa-arrow-right fa-rotate-by" style="--fa-rotate-angle: 330deg;"></i>
-                                </span>
-                        </button>
-                    </form>
+                    </div>
+                </div>
+                <div class="tile is-child box notification is-light">
+                    <p class="title is-5">Commandes de la base roulante</p>
+                    <div class="columns is-vcentered is-gapless">
+                        <div class="column">
+                            <form method="POST" id="form_4">
+                                @csrf
+                                <button class="button is-link btn_form is-medium" id="btn_4" type="submit">
+                                    <span class="icon">
+                                        <i class="fa-solid fa-arrow-rotate-right"></i>
+                                    </span>
+                                </button>
+                            </form>
+                        </div>
+                        <div class="column">
+                            <form method="POST" id="form_5">
+                                @csrf
+                                <button class="button is-link btn_form is-medium" id="btn_3" type="submit">
+                                    <span class="icon">
+                                        <i class="fa-solid fa-arrow-right fa-rotate-by" style="--fa-rotate-angle: 210deg;"></i>
+                                        </span>
+                                </button>
+                            </form>
+                            <form method="POST" id="form_6">
+                                @csrf
+                                <button class="button is-link btn_form is-medium" id="btn_3" type="submit">
+                                    <span class="icon">
+                                        <i class="fa-solid fa-arrow-right fa-rotate-by" style="--fa-rotate-angle: 150deg;"></i>
+                                        </span>
+                                </button>
+                            </form>
+                        </div>
+                        <div class="column">
+                            <form method="POST" id="form_7">
+                                @csrf
+                                <button class="button is-link btn_form is-medium" id="btn_3" type="submit">
+                                    <span class="icon">
+                                        <i class="fa-solid fa-arrow-right fa-rotate-by" style="--fa-rotate-angle: 270deg;"></i>
+                                        </span>
+                                </button>
+                            </form>
+                            <form method="POST" id="form_8">
+                                @csrf
+                                <button class="button is-link btn_form is-medium" id="btn_3" type="submit">
+                                    <span class="icon">
+                                        <i class="fa-solid fa-arrow-right fa-rotate-by" style="--fa-rotate-angle: 90deg;"></i>
+                                        </span>
+                                </button>
+                            </form>
+                        </div>
+                        <div class="column">
+                            <form method="POST" id="form_9">
+                                @csrf
+                                <button class="button is-link btn_form is-medium" id="btn_3" type="submit">
+                                    <span class="icon">
+                                        <i class="fa-solid fa-arrow-right fa-rotate-by" style="--fa-rotate-angle: 330deg;"></i>
+                                        </span>
+                                </button>
+                            </form>
+                            <form method="POST" id="form_10">
+                                @csrf
+                                <button class="button is-link btn_form is-medium" id="btn_3" type="submit">
+                                    <span class="icon">
+                                        <i class="fa-solid fa-arrow-right fa-rotate-by" style="--fa-rotate-angle: 30deg;"></i>
+                                        </span>
+                                </button>
+                            </form>
+                        </div>
+                        <div class="column">
+                            <form method="POST" id="form_11">
+                                @csrf
+                                <button class="button is-link btn_form is-medium" id="btn_4" type="submit">
+                                    <span class="icon">
+                                        <i class="fa-solid fa-arrow-rotate-left"></i>
+                                    </span>
+                                </button>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+                <div class="tile is-child box notification is-light">
+                    <p class="title is-5">Commandes des actionneurs</p>
                 </div>
             </div>
-
-            <div class="buttons">
-                <form method="POST" id="form_7">
-                    @csrf
-                    <button class="button is-link btn_form" id="btn_3" type="submit">
-                        <span class="icon">
-                            <i class="fa-solid fa-arrow-right fa-rotate-by" style="--fa-rotate-angle: 150deg;"></i>
-                            </span>
-                    </button>
-                </form>
-                <form method="POST" id="form_8">
-                    @csrf
-                    <button class="button is-link btn_form" id="btn_3" type="submit">
-                        <span class="icon">
-                            <i class="fa-solid fa-arrow-right fa-rotate-by" style="--fa-rotate-angle: 90deg;"></i>
-                            </span>
-                    </button>
-                </form>
-                <form method="POST" id="form_9">
-                    @csrf
-                    <button class="button is-link btn_form" id="btn_3" type="submit">
-                        <span class="icon">
-                            <i class="fa-solid fa-arrow-right fa-rotate-by" style="--fa-rotate-angle: 30deg;"></i>
-                            </span>
-                    </button>
-                </form>
-            </div>
-
-
-            
-          </div>
-          <div class="tile is-child box notification is-light">
-            <p class="title is-5">Commandes de contôle</p>
-          </div>
         </div>
-        <div class="tile is-parent">
-          <div class="tile is-child box notification is-light">
+        <div class="tile is-parent is-4">
+            <div class="tile is-child box notification is-light">
             <span class="title is-5 mb-3">
                 Console
             </span>
             <div id="logs_console">
-                <pre class="has-background-black logs" style="height:400px; width:780px !important; overflow: scroll;"><?php if(count($logs) > 0) { ?>@foreach($logs as $log)<?php $log->response = str_replace("\\r", "\r", $log->response); ?>
-<div class="tags has-addons"><span class="tag icon is-black"><i class="fa-solid fa-caret-right" id="log_icon_<?= $log->id ?>" onclick="showCompleteLog('{{ $log->id }}');"></i></span> <span class="tag is-dark">[{{ $log->created_at->format("d/m/Y") }} à {{ $log->created_at->format("H:i:s") }}]</span><span class="tag is-info"><strong>{{ $log->command_name }}</strong></span><span class="tag <?php if($log->state == 0) echo "is-success"; else echo "is-danger"; ?>"><strong>{{ $log->state }}</strong></span></div><table class="table is-narrow has-text-centered is-fullwidth is-bordered has-background-grey-darker has-text-light" id="log_reponse_<?= $log->id ?>" style="display: none"><tbody><tr class="has-background-link-dark"><td>ID</td><td>Data</td><td>Status</td></tr><?php $datas = json_decode($log->response);?>@foreach($datas as $data)<tr><td class="has-background-grey-dark">{{ $data->{"id"} }}</td><td>{{ $data->{"data"} }}</td><td class="<?php if($log->state == 0) echo "has-background-success"; else echo "has-background-danger"; ?>"><strong>{{ $data->{"status"} }}</strong></td></tr>@endforeach</tbody></table>@endforeach
+                <pre class="has-background-black logs" style="height:400px; width:680px !important; overflow: scroll;"><?php if(count($logs) > 0) { ?>@foreach($logs as $log)<?php $log->response = str_replace("\\r", "\r", $log->response); ?>
+                    <div class="tags has-addons"><span class="tag icon is-black"><i class="fa-solid fa-caret-right" id="log_icon_<?= $log->id ?>" onclick="showCompleteLog('{{ $log->id }}');"></i></span> <span class="tag is-dark">[{{ $log->created_at->format("d/m/Y") }} à {{ $log->created_at->format("H:i:s") }}]</span><span class="tag is-info"><strong>{{ $log->command_name }}</strong></span><span class="tag <?php if($log->state == 0) echo "is-success"; else echo "is-danger"; ?>"><strong>{{ $log->state }}</strong></span></div><table class="table is-narrow has-text-centered is-fullwidth is-bordered has-background-grey-darker has-text-light" id="log_reponse_<?= $log->id ?>" style="display: none"><tbody><tr class="has-background-link-dark"><td>ID</td><td>Data</td><td>Status</td></tr><?php $datas = json_decode($log->response);?>@foreach($datas as $data)<tr><td class="has-background-grey-dark">{{ $data->{"id"} }}</td><td>{{ $data->{"data"} }}</td><td class="<?php if($log->state == 0) echo "has-background-success"; else echo "has-background-danger"; ?>"><strong>{{ $data->{"status"} }}</strong></td></tr>@endforeach</tbody></table>@endforeach
                     <?php } else { ?><span class="tag is-dark">Aucun log pour le moment, veuillez sélectionner une action pour commencer</span><?php } ?>
                 </pre>
             </div>
@@ -138,8 +161,8 @@
             </div>
           </div>
         </div>
-      </div>
-      <div class="pageloader is-link"><span class="title">L'action est en cours de traitement, veuillez patienter...</span></div>
+    </div>
+    <div class="pageloader is-link"><span class="title">L'action est en cours de traitement, veuillez patienter...</span></div>
     <script>
     $(document).ready(function () {
         logUrl = "{{ route('log') }}";
