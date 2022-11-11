718898f3363fc18438a7c181bae8d06b4f806ab4
HeatSinkRu <scditte@pm.me>
2022-11-11 21:25:14 +0100

    improve dashboard

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -3,7 +3,7 @@
     <x-slot name="title"> @lang('Dashboard') </x-slot>
 
 
-    <div class="columns has-background-white">
+    <div class="columns">
         <div class="column is-full">
             <div id="logs_console">
                 <span class="title is-5">
@@ -29,71 +29,55 @@
         </div>
     </div>
 
-    <div class="tile is-ancestor">
-        <div class="tile is-4">
-            <div class="tile is-parent is-vertical">
-                <div class="tile is-child box notification is-light">
-                    <p class="title is-5">Commandes de diagnostic</p>
-                    <div class="buttons">
-
-                        <x-button title="Vérification de la connectivité" id="1" icon="fa-solid fa-tower-cell" url="{{ route('log') }}"/>
-                        <x-button title="Avancer le robot" id="2" icon="fa-solid fa-gamepad" url="{{ route('log') }}"/>
-                        <x-button title="Position du robot" id="3" icon="fa-solid fa-crosshairs" url="{{ route('log') }}"/>
-
-                    </div>
+    <div class="columns mt-4">
+        <div class="column is-one-third box">
+            <p class="title is-5">Commandes de diagnostic</p>
+                <div class="buttons">
+                    <x-button title="Vérification de la connectivité" id="1" icon="fa-solid fa-tower-cell" url="{{ route('log') }}"/>
+                    <x-button title="Avancer le robot" id="2" icon="fa-solid fa-gamepad" url="{{ route('log') }}"/>
+                    <x-button title="Position du robot" id="3" icon="fa-solid fa-crosshairs" url="{{ route('log') }}"/>
+             </div>
+        </div>
+        <div class="column is-one-third box">
+            <p class="title is-5">Commandes de la base roulante</p>
+            <div class="columns is-vcentered is-gapless">
+                <div class="column">
+                    <x-button id="4" icon="fa-solid fa-arrow-rotate-right" url="{{ route('log') }}"/>
+                </div>
+                <div class="column">
+                    <x-button id="5" icon="fa-solid fa-arrow-right fa-rotate-by" url="{{ route('log') }}" icon_style="--fa-rotate-angle: 210deg;"/>
+                    <x-button id="6" icon="fa-solid fa-arrow-right fa-rotate-by" url="{{ route('log') }}" icon_style="--fa-rotate-angle: 150deg;"/>
+                </div>
+                <div class="column">
+                    <x-button id="7" icon="fa-solid fa-arrow-right fa-rotate-by" url="{{ route('log') }}" icon_style="--fa-rotate-angle: 270deg;"/>
+                    <x-button id="8" icon="fa-solid fa-arrow-right fa-rotate-by" url="{{ route('log') }}" icon_style="--fa-rotate-angle: 90deg;"/>
+                </div>
+                <div class="column">
+                    <x-button id="9" icon="fa-solid fa-arrow-right fa-rotate-by" url="{{ route('log') }}" icon_style="--fa-rotate-angle: 330deg;"/>
+                    <x-button id="10" icon="fa-solid fa-arrow-right fa-rotate-by" url="{{ route('log') }}" icon_style="--fa-rotate-angle: 30deg;"/>
                 </div>
 
-                <div class="tile is-child box notification is-light">
-                    <p class="title is-5">Commandes de la base roulante</p>
-                    <div class="columns is-vcentered is-gapless">
-                        <div class="column">
-                            <x-button id="4" icon="fa-solid fa-arrow-rotate-right" url="{{ route('log') }}"/>
-                        </div>
-                        <div class="column">
-                            <x-button id="5" icon="fa-solid fa-arrow-right fa-rotate-by" url="{{ route('log') }}" icon_style="--fa-rotate-angle: 210deg;"/>
-                            <x-button id="6" icon="fa-solid fa-arrow-right fa-rotate-by" url="{{ route('log') }}" icon_style="--fa-rotate-angle: 150deg;"/>
-                        </div>
-                        <div class="column">
-                            <x-button id="7" icon="fa-solid fa-arrow-right fa-rotate-by" url="{{ route('log') }}" icon_style="--fa-rotate-angle: 270deg;"/>
-                            <x-button id="8" icon="fa-solid fa-arrow-right fa-rotate-by" url="{{ route('log') }}" icon_style="--fa-rotate-angle: 90deg;"/>
-                        </div>
-                        <div class="column">
-                            <x-button id="9" icon="fa-solid fa-arrow-right fa-rotate-by" url="{{ route('log') }}" icon_style="--fa-rotate-angle: 330deg;"/>
-                            <x-button id="10" icon="fa-solid fa-arrow-right fa-rotate-by" url="{{ route('log') }}" icon_style="--fa-rotate-angle: 30deg;"/>
-                        </div>
-
-                        <div class="column">
-                            <x-button id="11" icon="fa-solid fa-arrow-rotate-left" url="{{ route('log') }}"/>
-                        </div>
-                    </div>
-                </div>
-                <div class="tile is-child box notification is-light">
-                    <p class="title is-5">Commandes des actionneurs</p>
-                </div>
-                <div class="tile is-child box notification is-light">
-                    <p class="title is-5">Alimentation générale</p>
-                    <div class="columns is-vcentered">
-                        <div class="column">
-                            <span class="icon is-large fa-3x has-text-success">
-                                <i class="fas fa-check-square"></i>
-                            </span>
-                        </div>
-                        <div class="column">
-                            <x-button title="On" id="12" url="{{ route('log') }}"/>
-                        </div>
-
-                        <div class="column">
-                            <x-button title="Off" id="13" url="{{ route('log') }}"/>
-                        </div>
-                    </div>
+                <div class="column">
+                    <x-button id="11" icon="fa-solid fa-arrow-rotate-left" url="{{ route('log') }}"/>
                 </div>
             </div>
         </div>
-        <div class="tile is-parent is-8">
-            <div class="tile is-child box notification is-light">
+        <div class="column is-one-third box">
+            <p class="title is-5">Alimentation générale</p>
+            <div class="columns is-vcentered">
+                <div class="column">
+                    <span class="icon is-large fa-3x has-text-success">
+                        <i class="fas fa-check-square"></i>
+                    </span>
+                </div>
+                <div class="column">
+                    <x-button title="On" id="12" url="{{ route('log') }}"/>
+                </div>
 
-
-          </div>
+                <div class="column">
+                    <x-button title="Off" id="13" url="{{ route('log') }}"/>
+                </div>
+            </div>
         </div>
     </div>
     <div class="pageloader is-link"><span class="title">L'action est en cours de traitement, veuillez patienter...</span></div>
