7f0f200965417233d3971dee66f7d986eee1a285
OtarieSupreme <gaby.mouchette@gmail.com>
2022-11-18 23:22:29 +0100

    Correction script startup

--- startup.sh
+++ startup.sh
@@ -1,7 +1,7 @@
-echo "Defineing pin state"
+echo "Defining pin state"
 gpio mode 3 out
 gpio mode 2 in
 echo "Setting up CAN interface"
 sudo ip link set can0 up type can bitrate 181818 loopback off
-echo "Staring Laravel server"
+echo "Starting Laravel server"
 php artisan serve --host 0.0.0.0 --port=8000
