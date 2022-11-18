1f2d68a07a69710cb2092d873dd874566c3e9224
OtarieSupreme <gaby.mouchette@gmail.com>
2022-11-18 22:05:03 +0100

    Ajout d'un script pour le démarrage

---
+++ startup.sh
@@ -0,0 +1,7 @@
+echo "Defineing pin state"
+gpio mode 3 out
+gpio mode 2 in
+echo "Setting up CAN interface"
+sudo ip link set can0 up type can bitrate 181818 loopback off
+echo "Staring Laravel server"
+php artisan serve --host 0.0.0.0 --port=8000
