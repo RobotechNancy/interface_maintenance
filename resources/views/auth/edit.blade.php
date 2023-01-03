5416e073d2c3ee9020b7ee800421133362628a17
HeatSink Ru <heatsink.ru@gmail.com>
2023-01-03 18:36:23 +0100

    correction de l'affichage sous windows

--- resources/views/auth/edit.blade.php
+++ resources/views/auth/edit.blade.php
@@ -1,169 +1,176 @@
 <x-app-layout>
     <x-slot name="title"> @lang('Profile edition') </x-slot>
 
-    <h4 class="mb-2 ms-3">Formulaire de modification du compte "{{ $user->name }}"</h4>
+    <div class="row">
+        <div class="col-1 d-none d-md-block"></div>
+        <div class="col ms-lg-4">
 
-    <p class="ps-3 text-muted"><small><i class="fa-solid fa-circle-info"></i> Dernière modification de ce profil le {{ $user->updated_at->format('d/m/Y') }} à
-        {{ $user->updated_at->format('H:i:s') }}.</small></p>
+            <h4 class="mb-2 ms-3">Formulaire de modification du compte "{{ $user->name }}"</h4>
 
-    <form method="POST" action="{{ route('update', ['user' => $user]) }}" class="p-3">
-        @csrf
+            <p class="ps-3 text-muted"><small><i class="fa-solid fa-circle-info"></i> Dernière modification de ce profil
+                    le {{ $user->updated_at->format('d/m/Y') }} à
+                    {{ $user->updated_at->format('H:i:s') }}.</small></p>
 
-        <div class="row">
-            <div class="col">
+            <form method="POST" action="{{ route('update', ['user' => $user]) }}" class="p-3">
+                @csrf
 
-                <div class="input-group mb-4">
-                    <span class="input-group-text" id="user"><i class="fas fa-user"></i></span>
+                <div class="row">
+                    <div class="col">
 
-                    <div class="form-floating <?php if (!empty($errors->get('name'))) {
-                        echo 'is-invalid';
-                    } ?>">
+                        <div class="input-group mb-4">
+                            <span class="input-group-text" id="user"><i class="fas fa-user"></i></span>
 
-                        <input class="form-control bg-dark text-white <?php if (!empty($errors->get('name'))) {
-                            echo 'is-invalid';
-                        } ?>" type="name" id="name"
-                            name="name" value="{{ $user->name }}" placeholder="Alex" aria-describedby="user"
-                         autofocus>
+                            <div class="form-floating <?php if (!empty($errors->get('name'))) {
+                                echo 'is-invalid';
+                            } ?>">
 
-                        <label for="name" class="form-label">{{ __('Name') }}</label>
+                                <input class="form-control bg-dark text-white <?php if (!empty($errors->get('name'))) {
+                                    echo 'is-invalid';
+                                } ?>" type="name"
+                                    id="name" name="name" value="{{ $user->name }}" placeholder="Alex"
+                                    aria-describedby="user" autofocus>
 
+                                <label for="name" class="form-label">{{ __('Name') }}</label>
+
+                            </div>
+
+                            <x-input-error :messages="$errors->get('name')" />
+
+                        </div>
                     </div>
 
-                    <x-input-error :messages="$errors->get('name')" />
+                    <div class="w-100 d-sm-none"></div>
 
-                </div>
-            </div>
+                    <div class="col">
 
-            <div class="w-100 d-sm-none"></div>
+                        <div class="input-group mb-4">
+                            <span class="input-group-text" id="envelope"><i class="fas fa-envelope"></i></span>
+                            <div class="form-floating <?php if (!empty($errors->get('email'))) {
+                                echo 'is-invalid';
+                            } ?>">
+                                <input class="form-control bg-dark text-white <?php if (!empty($errors->get('email'))) {
+                                    echo 'is-invalid';
+                                } ?>" type="email"
+                                    id="email" name="email" value="{{ $user->email }}"
+                                    placeholder="alex@example.com" aria-describedby="envelope">
 
-            <div class="col">
+                                <label for="email" class="form-label">{{ __('Email') }}</label>
+                            </div>
+                            <x-input-error :messages="$errors->get('email')" />
 
-                <div class="input-group mb-4">
-                    <span class="input-group-text" id="envelope"><i class="fas fa-envelope"></i></span>
-                    <div class="form-floating <?php if (!empty($errors->get('email'))) {
-                        echo 'is-invalid';
-                    } ?>">
-                        <input class="form-control bg-dark text-white <?php if (!empty($errors->get('email'))) {
-                            echo 'is-invalid';
-                        } ?>" type="email" id="email"
-                            name="email" value="{{ $user->email }}" placeholder="alex@example.com"
-                            aria-describedby="envelope">
-
-                        <label for="email" class="form-label">{{ __('Email') }}</label>
+                        </div>
                     </div>
-                    <x-input-error :messages="$errors->get('email')" />
-
                 </div>
-            </div>
-        </div>
 
-        <div class="mb-4">
-            <div class="form-floating">
-                <select class="form-select bg-dark text-white <?php if (!empty($errors->get('role'))) {
-                    echo 'is-invalid';
-                } ?>" id="role" name="role"
+                <div class="mb-4">
+                    <div class="form-floating">
+                        <select class="form-select bg-dark text-white <?php if (!empty($errors->get('role'))) {
+                            echo 'is-invalid';
+                        } ?>" id="role" name="role"
+                            @if (!(Auth::user()->role == 2 && $user->role != 2)) disabled=true @endif>
 
-                    @if (!(Auth::user()->role == 2 && $user->role != 2)) disabled=true @endif>
+                            <option value=0 @if ($user->role == 0) selected @endif>Lecteur</option>
+                            <option value=1 @if ($user->role == 1) selected @endif>Editeur</option>
+                            <option value=2 @if ($user->role == 2) selected @endif disabled>Administrateur
+                            </option>
+                        </select>
 
-                    <option value=0 @if ($user->role == 0) selected @endif>Lecteur</option>
-                    <option value=1 @if ($user->role == 1) selected @endif>Editeur</option>
-                    <option value=2 @if ($user->role == 2) selected @endif disabled>Administrateur</option>
-                </select>
+                        <label for="role" class="label">{{ __('Rôle') }}</label>
+                    </div>
+                    <x-input-error :messages="$errors->get('role')" />
 
-                <label for="role" class="label">{{ __('Rôle') }}</label>
-            </div>
-            <x-input-error :messages="$errors->get('role')" />
+                    @if (Auth::user()->role != 2 && Auth::user()->id != $user->id)
+                        <p class="mt-2 text-warning"><small><i class="fa-solid fa-triangle-exclamation"></i> Il n'est
+                                pas possible de modifier le rôle de l'utilisateur car
+                                vous n'êtes pas administrateur.</small></p>
+                    @elseif (Auth::user()->role != 2 && Auth::user()->id == $user->id)
+                        <p class="mt-2 text-warning"><small><i class="fa-solid fa-triangle-exclamation"></i> Il n'est
+                                pas possible de modifier le rôle de votre compte car vous
+                                n'êtes pas administrateur.</small></p>
+                    @elseif ($user->role == 2 && Auth::user()->id != $user->id)
+                        <p class="mt-2 text-warning"><small><i class="fa-solid fa-triangle-exclamation"></i> Il n'est
+                                pas possible de modifier le rôle de l'utilisateur car il
+                                s'agit d'un compte administrateur.</small></p>
+                    @elseif ($user->role == 2 && Auth::user()->id == $user->id)
+                        <p class="mt-2 text-warning"><small><i class="fa-solid fa-triangle-exclamation"></i> Il n'est
+                                pas possible de modifier le rôle de votre compte car vous
+                                disposez déjà d'un compte administrateur.</small></p>
+                    @endif
+                </div>
 
-            @if (Auth::user()->role != 2 && Auth::user()->id != $user->id)
+                <div class="row">
+                    <div class="col">
 
-            <p class="mt-2 text-warning"><small><i class="fa-solid fa-triangle-exclamation"></i> Il n'est pas possible de modifier le rôle de l'utilisateur car
-                    vous n'êtes pas administrateur.</small></p>
+                        <div class="input-group mb-4">
+                            <span class="input-group-text" id="lock"><i class="fas fa-lock"></i></span>
 
-            @elseif (Auth::user()->role != 2 && Auth::user()->id == $user->id)
+                            <div class="form-floating <?php if (!empty($errors->get('password'))) {
+                                echo 'is-invalid';
+                            } ?>">
+                                <input class="form-control bg-dark text-white <?php if (!empty($errors->get('password'))) {
+                                    echo 'is-invalid';
+                                } ?>" type="password"
+                                    id="password" name="password" autocomplete="new-password" placeholder="*********"
+                                    aria-describedby="lock">
 
-            <p class="mt-2 text-warning"><small><i class="fa-solid fa-triangle-exclamation"></i> Il n'est pas possible de modifier le rôle de votre compte car vous
-                    n'êtes pas administrateur.</small></p>
+                                <label for="password" class="form-label">{{ __('Password') }}</label>
 
-            @elseif ($user->role == 2 && Auth::user()->id != $user->id)
+                            </div>
+                            <x-input-error :messages="$errors->get('password')" />
 
-            <p class="mt-2 text-warning"><small><i class="fa-solid fa-triangle-exclamation"></i> Il n'est pas possible de modifier le rôle de l'utilisateur car il
-                    s'agit d'un compte administrateur.</small></p>
+                        </div>
+                    </div>
 
-            @elseif ($user->role == 2 && Auth::user()->id == $user->id)
+                    <div class="w-100 d-sm-none"></div>
 
-            <p class="mt-2 text-warning"><small><i class="fa-solid fa-triangle-exclamation"></i> Il n'est pas possible de modifier le rôle de votre compte car vous
-                    disposez déjà d'un compte administrateur.</small></p>
+                    <div class="col">
+
+                        <div class="input-group mb-5">
+                            <span class="input-group-text" id="lock"><i class="fas fa-lock"></i></span>
+
+                            <div class="form-floating <?php if (!empty($errors->get('password_confirmation'))) {
+                                echo 'is-invalid';
+                            } ?>">
+                                <input class="form-control bg-dark text-white <?php if (!empty($errors->get('password_confirmation'))) {
+                                    echo 'is-invalid';
+                                } ?>" type="password"
+                                    id="password_confirmation" name="password_confirmation" autocomplete="new-password"
+                                    placeholder="*********" aria-describedby="lock">
+
+                                <label for="password_confirmation"
+                                    class="form-label">{{ __('Confirm Password') }}</label>
+
+                            </div>
+
+                            <x-input-error :messages="$errors->get('password_confirmation')" />
+
+                        </div>
+                    </div>
+                </div>
+
+                <div class="btn-group" role="group">
+                    <button type="submit" class="btn btn-primary">Enregistrer les modifications <i
+                            class="fa-solid fa-floppy-disk"></i></button>
+                </div>
+            </form>
+
+
+            <form method="POST" action="{{ route('delete', ['user' => $user]) }}" class="d-none" id="form_delete">
+                @csrf
+            </form>
+
+            <a role="button" class="btn btn-danger ms-3" onclick="event.preventDefault(); $('#form_delete').submit();">
+                Supprimer l'utilisateur <i class="fa-solid fa-trash-can"></i>
+            </a>
+
+            @if (session()->has('success'))
+                <x-notification title="Modification du profil" color="text-bg-success">{{ session('success') }}
+                </x-notification>
+            @endif
+            @if (session()->has('warning'))
+                <x-notification title="Modification du profil" color="text-bg-warning">{{ session('warning') }}
+                </x-notification>
             @endif
         </div>
-
-        <div class="row">
-            <div class="col">
-
-                <div class="input-group mb-4">
-                    <span class="input-group-text" id="lock"><i class="fas fa-lock"></i></span>
-
-                    <div class="form-floating <?php if (!empty($errors->get('password'))) {
-                        echo 'is-invalid';
-                    } ?>">
-                        <input class="form-control bg-dark text-white <?php if (!empty($errors->get('password'))) {
-                            echo 'is-invalid';
-                        } ?>" type="password"
-                            id="password" name="password" autocomplete="new-password" placeholder="*********"
-                            aria-describedby="lock">
-
-                        <label for="password" class="form-label">{{ __('Password') }}</label>
-
-                    </div>
-                    <x-input-error :messages="$errors->get('password')" />
-
-                </div>
-            </div>
-
-            <div class="w-100 d-sm-none"></div>
-
-            <div class="col">
-
-                <div class="input-group mb-5">
-                    <span class="input-group-text" id="lock"><i class="fas fa-lock"></i></span>
-
-                    <div class="form-floating <?php if (!empty($errors->get('password_confirmation'))) {
-                        echo 'is-invalid';
-                    } ?>">
-                        <input class="form-control bg-dark text-white <?php if (!empty($errors->get('password_confirmation'))) {
-                            echo 'is-invalid';
-                        } ?>" type="password"
-                            id="password_confirmation" name="password_confirmation" autocomplete="new-password"
-                            placeholder="*********" aria-describedby="lock">
-
-                        <label for="password_confirmation" class="form-label">{{ __('Confirm Password') }}</label>
-
-                    </div>
-
-                    <x-input-error :messages="$errors->get('password_confirmation')" />
-
-                </div>
-            </div>
-        </div>
-
-        <div class="btn-group" role="group">
-            <button type="submit" class="btn btn-primary">Enregistrer les modifications <i class="fa-solid fa-floppy-disk"></i></button>
-        </div>
-    </form>
-
-
-    <form method="POST" action="{{ route('delete', ['user' => $user]) }}" class="d-none" id="form_delete">
-        @csrf
-    </form>
-
-    <a role="button" class="btn btn-danger ms-3"
-    onclick="event.preventDefault(); $('#form_delete').submit();">
-        Supprimer l'utilisateur <i class="fa-solid fa-trash-can"></i>
-    </a>
-
-    @if (session()->has('success'))
-        <x-notification title="Modification du profil" color="text-bg-success">{{ session('success') }}</x-notification>
-    @endif
-    @if (session()->has('warning'))
-        <x-notification title="Modification du profil" color="text-bg-warning">{{ session('warning') }}</x-notification>
-    @endif
+    </div>
 </x-app-layout>
--- resources/views/auth/index.blade.php
+++ resources/views/auth/index.blade.php
@@ -1,84 +1,111 @@
 <x-app-layout>
     <x-slot name="title"> @lang('Users list') </x-slot>
-    <h4 class="mb-2 ms-3">Liste des utilisateurs du site</h4>
 
-    <div class="row row-cols-1 row-cols-md-2 row-cols-lg-3 g-4 p-3">
+    <div class="row">
+        <div class="col-1 d-none d-md-block"></div>
+        <div class="col ms-lg-4">
 
-        @foreach ($users as $user)
-            <?php
-            $role = '';
-            switch ($user->role) {
-                case 1:
-                    $role = 'Editeur 🥈';
-                    break;
+            <h4 class="mb-2 ms-3">Liste des utilisateurs du site</h4>
+            <div class="row row-cols-1 row-cols-md-2 row-cols-lg-3 g-4 p-3">
 
-                case 2:
-                    $role = 'Administrateur 🥇';
-                    break;
+                @foreach ($users as $user)
+                    <?php
+                    $role = '';
+                    switch ($user->role) {
+                        case 1:
+                            $role = 'Editeur 🥈';
+                            break;
 
-                default:
-                    $role = 'Lecteur 🥉';
-                    break;
-            }
-            ?>
+                        case 2:
+                            $role = 'Administrateur 🥇';
+                            break;
 
-            <div class="col">
-                <div aria-hidden="true" class="card text-bg-dark border border-light border-opacity-25">
-                    <div class="card-body">
-                        <h5 class="card-title">
-                            <span>{{ $user->name }}
+                        default:
+                            $role = 'Lecteur 🥉';
+                            break;
+                    }
+                    ?>
 
-                                @if(Cache::has('user-is-online-' . $user->email))
-                                    <span class="text-success fs-6 ms-2 fst-italic"><small>En ligne</small></span>
-                                @else
-                                    <span class="text-danger fs-6 ms-2 fst-italic"><small>Déconnecté</small></span>
-                                @endif
+                    <div class="col">
+                        <div aria-hidden="true" class="card text-bg-dark border border-light border-opacity-25">
+                            <div class="card-body">
+                                <h5 class="card-title">
+                                    <span>{{ $user->name }}
+
+                                        @if (Cache::has('user-is-online-' . $user->email))
+                                            <span class="text-success fs-6 ms-2 fst-italic"><small>En
+                                                    ligne</small></span>
+                                        @else
+                                            <span
+                                                class="text-danger fs-6 ms-2 fst-italic"><small>Déconnecté</small></span>
+                                        @endif
 
 
-                                @if(Auth::user()->id == $user->id)
-                                    <span class="badge rounded-pill ms-2 text-bg-info"><small>Mon compte</small></span>
-                                @endif
-                            </span>
-                        </h5>
-                        <h6 class="card-subtitle pt-2 text-muted">
-                            <small>
-                                <i class="fa-solid fa-circle-info"></i> Profil modifié le
-                                {{ $user->updated_at->format('d/m/Y') }} à
-                                {{ $user->updated_at->format('H:i:s') }}
-                            </small>
-                        </h6>
-                    </div>
+                                        @if (Auth::user()->id == $user->id)
+                                            <span class="badge rounded-pill ms-2 text-bg-info"><small>Mon
+                                                    compte</small></span>
+                                        @endif
+                                    </span>
+                                </h5>
+                                <h6 class="card-subtitle pt-2 text-muted">
+                                    <small>
+                                        <i class="fa-solid fa-circle-info"></i> Profil modifié le
+                                        {{ $user->updated_at->format('d/m/Y') }} à
+                                        {{ $user->updated_at->format('H:i:s') }}
+                                    </small>
+                                </h6>
+                            </div>
 
-                    <ul class="list-group list-group-flush">
-                        <li class="list-group-item text-bg-dark border border-white border-opacity-10"><i
-                                class="fa-solid fa-id-card-clip pe-1"></i> Identifiant : {{ $user->id }}</li>
-                        <li class="list-group-item text-bg-dark border border-white border-opacity-25"><i
-                                class="fa-solid fa-at pe-1"></i> @lang('Email') : <a
-                                href="mailto:{{ $user->email }}" class="text-white" style="text-decoration: none;">{{ $user->email }}</a>
-                        </li>
-                        <li class="list-group-item text-bg-dark border border-white border-opacity-10"><i
-                                class="fa-solid fa-ranking-star pe-1"></i> @lang('Role') : {{ $role }}</li>
-                    </ul>
+                            <ul class="list-group list-group-flush">
+                                <li class="list-group-item text-bg-dark border border-white border-opacity-10"><i
+                                        class="fa-solid fa-id-card-clip pe-1"></i> Identifiant : {{ $user->id }}
+                                </li>
+                                <li class="list-group-item text-bg-dark border border-white border-opacity-25"><i
+                                        class="fa-solid fa-at pe-1"></i> @lang('Email') : <a
+                                        href="mailto:{{ $user->email }}" class="text-white"
+                                        style="text-decoration: none;">{{ $user->email }}</a>
+                                </li>
+                                <li class="list-group-item text-bg-dark border border-white border-opacity-10"><i
+                                        class="fa-solid fa-ranking-star pe-1"></i> @lang('Role') :
+                                    {{ $role }}</li>
+                            </ul>
 
-                    @if (Auth::user()->role == 2 || $user->id == Auth::user()->id)
-                        <div class="card-footer text-center m-2">
-                            <a class="btn btn-sm btn-primary" href="{{ route('edit', ['id' => $user->id]) }}"><i
-                                    class="fa-solid fa-pen-to-square"></i> Éditer <span class="d-none d-lg-inline">@if(Auth::user()->id == $user->id) mon @else le @endif profil</span></a>
+                            @if (Auth::user()->role == 2 || $user->id == Auth::user()->id)
+                                <div class="card-footer text-center m-2">
+                                    <a class="btn btn-sm btn-primary" href="{{ route('edit', ['id' => $user->id]) }}"><i
+                                            class="fa-solid fa-pen-to-square"></i> Éditer <span
+                                            class="d-none d-lg-inline">
+                                            @if (Auth::user()->id == $user->id)
+                                                mon
+                                            @else
+                                                le
+                                            @endif profil
+                                        </span></a>
 
-                            <form id="delete{{ $user->id }}" action="{{ route('delete', ['user' => $user]) }}"
-                                method="POST" style="display: none;">
-                                @csrf
-                            </form>
+                                    <form id="delete{{ $user->id }}"
+                                        action="{{ route('delete', ['user' => $user]) }}" method="POST"
+                                        style="display: none;">
+                                        @csrf
+                                    </form>
 
-                            <a class="btn btn-sm btn-danger ms-2"
-                                onclick="event.preventDefault(); document.getElementById('delete{{ $user->id }}').submit();"><i
-                                    class="fa-solid fa-trash-can"></i> Supprimer <span class="d-none d-lg-inline">@if(Auth::user()->id == $user->id) mon compte @else l'utilisateur @endif</span></a>
+                                    <a class="btn btn-sm btn-danger ms-2"
+                                        onclick="event.preventDefault(); document.getElementById('delete{{ $user->id }}').submit();"><i
+                                            class="fa-solid fa-trash-can"></i> Supprimer <span
+                                            class="d-none d-lg-inline">
+                                            @if (Auth::user()->id == $user->id)
+                                                mon compte
+                                            @else
+                                                l'utilisateur
+                                            @endif
+                                        </span></a>
+                                </div>
+                            @endif
                         </div>
-                    @endif
-                </div>
-            </div>
-        @endforeach
+                    </div>
+                @endforeach
 
+            </div>
+        </div>
     </div>
 </x-app-layout>
 @if (session()->has('message'))
